d31ad806492d1a01f2218ea78898401f4940c590
zia <jzljzljl@outlook.com>
2017-08-03 17:33:10 +0800

    主页cardview阴影效果，点击波浪效果

--- app/src/main/java/com/mredrock/freshmanspecial/view/MainActivity.java
+++ app/src/main/java/com/mredrock/freshmanspecial/view/MainActivity.java
@@ -1,13 +1,10 @@
 package com.mredrock.freshmanspecial.view;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ImageView;
-
 import com.mredrock.freshmanspecial.R;
 import com.mredrock.freshmanspecial.Units.BaseActivity;
 import com.mredrock.freshmanspecial.presenter.IMainPresenter;
@@ -15,15 +12,16 @@ import com.mredrock.freshmanspecial.presenter.MainPresenter;
 
 public class MainActivity extends BaseActivity{
 
-    private ImageView junxun,shuju;
+    //使用cardView按钮，有点击波纹效果
+    private CardView junxun,shuju,fengcai,gonglve;
     private IMainPresenter presenter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         presenter = new MainPresenter(this);
-        setClick();
         setToolbar();
+        setClick();
     }
 
     private void setClick() {
@@ -39,12 +37,26 @@ public class MainActivity extends BaseActivity{
                 presenter.gotoshujuActivity();
             }
         });
+        fengcai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //风采点击事件
+            }
+        });
+        gonglve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //攻略点击事件
+            }
+        });
     }
 
     @Override
     protected void initData() {
         junxun = $(R.id.main_junxun);
         shuju = $(R.id.main_shuju);
+        fengcai = $(R.id.main_fengcai);
+        gonglve = $(R.id.main_gonglve);
     }
 
     private void setToolbar(){
