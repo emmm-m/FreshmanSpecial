ccd3884f259e1bdf64e5d4235531444333d4d3b1
zia <jzljzljl@outlook.com>
2017-08-11 16:52:22 +0800

    重邮数据中按钮ripple效果，巨无聊

--- app/src/main/java/com/mredrock/freshmanspecial/view/MainActivity.java
+++ app/src/main/java/com/mredrock/freshmanspecial/view/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mredrock.freshmanspecial.view;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.CardView;
